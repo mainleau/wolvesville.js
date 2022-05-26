3077187524bf46e09857aaec67848201da7ab875
Lo <45238635+mainleau@users.noreply.github.com>
2022-05-27 00:27:24 +0200

    test: update to new changes

--- test/fetchClan.test.js
+++ test/fetchClan.test.js
@@ -5,6 +5,6 @@ new Client().login()
   .then(async client => {
     client.clans.fetchOwn().then(console.log).catch(() => {});
     client.clans.fetchById('04c08a95-9452-408e-b7a5-7023bde1cc72').then(console.log);
-    client.clans.query('La Banda').then(x => console.log(x.clans.first()));
+    client.clans.query('La Banda').then(x => console.log(x.first()));
     client.clans.fetchByPlayerId('86892899-2fa1-4e00-a0dc-975769abe9fb').then(console.log);
   });
